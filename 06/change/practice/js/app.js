0cb587d87fb57609f384d8de63cda1be0adc8be9
last_alice <iolite.kinseiseki18@gmail.com>
2020-09-08 11:52:00 +0900

    要素の追加

--- 06/change/practice/js/app.js
+++ 06/change/practice/js/app.js
@@ -3,3 +3,15 @@ practice.innerHTML =  '<h1>れんしゅう</h1>';
 practice.style.backgroundColor = '#999999';
 practice.style.fontSize = '30px';
 practice.style.color = '#FFFFFF';
+
+// 要素を追加
+let first = document.createElement('div');
+first.setAttribute('id', 'first');
+first.innerHTML = '<p>要素を追加</p>';
+practice.insertBefore(first, null);
+
+// さらに要素の追加
+let second = document.createElement('div');
+second.setAttribute('id', 'second');
+second.innerHTML = '<p>さらに要素を追加</p>';
+practice.insertBefore(second, first);
